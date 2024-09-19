8d760998618a49c37e8dc4b24a80dcc8e1dd3277
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-19 16:39:16 +0200

    Exercise3 done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         var scan = new Scanner(System.in);
         while(true){
             int num;
@@ -14,6 +15,8 @@ public class Main {
                         Exercise1();
                     case 2:
                         Exercise2();
+                    case 3:
+                        Exercise3();
                     default:
                         System.out.println();
                 }
@@ -24,6 +27,29 @@ public class Main {
             }
         }
     }
+    public static void Exercise3() throws IOException {
+        System.out.println("In this program Im reading the file Exercise3.txt and finding out the percentage of commented lines.");
+        var instream = new BufferedReader(new FileReader("src/Exercise3.txt"));
+        //C:\Users\jarit\Lexicon Växjö\JavaStreaming\StreamingJavaBookExercises\src\TextForExe3.java
+        //File file = new File("findingFilePathForExercise3.txt");
+        //System.out.println(file.getAbsoluteFile());
+        //file.createNewFile();
+        int rowNr = 0;
+        int rowsWithComment = 0;
+        while(true){
+            String row = instream.readLine();
+            if (row == null){
+                break;
+            }
+            if (row.contains("//")){
+                rowsWithComment++;
+            }
+            rowNr++;
+        }
+        double amount = 100 / rowNr * rowsWithComment;
+        System.out.println("Rows: " + rowNr + ", Rows with comment: " + rowsWithComment + ", percentage: " + amount + "%.");
+    }
+
     public static void Exercise2(){
         int max = Integer.MIN_VALUE;
         int num;
